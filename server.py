cd3b3f9efebd4bfd67b191504b38c4dbae1459f1
slrc1 <info@slrc1.comli.com>
2017-08-04 00:55:53 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,12 +1,15 @@
 # my server 
 import socket,sys,urllib
 from threading import Thread
+import datetime
 import os,urlparse
 import argparse
 import requests,time,json
 from time import sleep
+from firebase import firebase
 
 PORT = int(sys.argv[1])
+firebase = firebase.FirebaseApplication(os.environ['furl'], None)
 
 class Client(Thread):
     def __init__(self,cs,ca):
@@ -20,44 +23,20 @@ def handle(self,client,client_address):
     res = 'HTTP/1.1 200 OK\r\n'
     try:
         path = req.split(" ",3)[1][1:]
-        data = path
-        if (path.find("?") == -1 and path == '') or path.find('?') == 0:
-            data = index(path)
-            res = res+'Content-Type: text/html\r\n'
-        elif path.startswith("v"):
-            data = str(mvbs(path))
-            res = res+'Content-Type: text/plain\r\n'
-        elif path.startswith("x"):
-            data = str(x(path))
-            res = res+'Content-Type: text/plain\r\n'
+        data = datetime.datetime.now().strftime("%I:%M%p on %B %d, %Y")
+        xt = path[2:].split('&')
+        dt = dict()
+        for xtz in xt:
+            dtz = xtz.split('=')
+            if len(dtz) == 2:
+                dt[dtz[0]] = dtz[1]
+        firebase.post('/',{data:dt})    
+        res = res+'Content-Type: text/plain\r\n'
         res = res+'\r\n'+data
     except Exception as e:
         res = res+repr(e)#.replace('\n','<br>')
     client.sendall(res)
     client.close()
-
-def index(path):
-    with open('index.html') as f:
-        return f.read();
-    return ''
-
-def mvbs(path):
-    with open('main.vbs') as f:
-        return f.read();
-    return ''
-
-def x(path):
-    xt = path[2:].split('&')
-    dt = dict()
-    for xtz in xt:
-        dtz = xtz.split('=')
-        if len(dtz) == 2:
-            dt[dtz[0]] = dtz[1]
-    #return str(dt)
-    #t = dt['t'].replace('+', ' ')
-    
-    return "echo %TIME% %DATE% %USERNAME% %USERDOMAIN% %RANDOM% *KNUS* test"
-
 listen_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 listen_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 listen_socket.bind(('', PORT))
